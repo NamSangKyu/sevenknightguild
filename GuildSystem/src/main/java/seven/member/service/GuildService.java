12101ae6d5ac12c3700f3f5db36bfe95129846b8
NamsSangKyu <nam2626@gmail.com>
2015-04-15 12:02:29 +0900

    client 기능 debuging 및 sqlSession 처리

--- GuildSystem/src/main/java/seven/member/service/GuildService.java
+++ GuildSystem/src/main/java/seven/member/service/GuildService.java
@@ -1,16 +1,11 @@
 package seven.member.service;
 
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.ibatis.session.SqlSession;
 
-import seven.date.DateCustom;
 import seven.member.dao.GuildDAO;
 import seven.member.db.ConnectionFactory;
 import seven.member.vo.GuildMemberVO;
@@ -18,12 +13,19 @@ import seven.member.vo.InstancePackage;
 import seven.member.vo.MemberWarfaceScoreVO;
 
 public class GuildService {
-	private SqlSession sqlSession;
+	
 	public GuildService() {
 		super();
 		// TODO Auto-generated constructor stub
-		sqlSession = ConnectionFactory.getSession().openSession();
+		
 	}
+	/**
+	 * create sqlSession
+	 */
+	public SqlSession getSqlSession(){
+		return ConnectionFactory.getSession().openSession();
+	}
+	
 	
 	/**
 	 * 전체회원 조회
@@ -31,10 +33,12 @@ public class GuildService {
 	 * @throws Exception
 	 */
 	public List<GuildMemberVO> selectAllMember() throws Exception{
-		List<GuildMemberVO> list = sqlSession.getMapper(GuildDAO.class).selectAllMember();
+		SqlSession session = getSqlSession();
+		List<GuildMemberVO> list = session.getMapper(GuildDAO.class).selectAllMember();
 		if(list.size() == 0){
 			throw new Exception("회원이 아무도 없습니다.");
 		}
+		session.close();
 		return list;
 	}
 	/**
@@ -44,15 +48,19 @@ public class GuildService {
 	 * @throws Exception
 	 */
 	public GuildMemberVO selectMember(String nick) throws Exception{
-		GuildMemberVO vo = sqlSession.getMapper(GuildDAO.class).selectMemberNick(nick);
+		SqlSession sqlSession = getSqlSession();
+		GuildMemberVO vo = sqlSession .getMapper(GuildDAO.class).selectMemberNick(nick);
 		if(vo == null)
 			throw new Exception("찾는 회원이 아무도 없습니다.");
+		sqlSession.close();
 		return vo;
 	}
 	public GuildMemberVO selectMember(int code) throws Exception{
-		GuildMemberVO vo = sqlSession.getMapper(GuildDAO.class).selectMemberCode(code);
+		SqlSession sqlSession = getSqlSession();
+		GuildMemberVO vo = sqlSession .getMapper(GuildDAO.class).selectMemberCode(code);
 		if(vo == null)
 			throw new Exception("찾는 회원이 아무도 없습니다.");
+		sqlSession.close();
 		return vo;
 	}
 	
@@ -64,10 +72,12 @@ public class GuildService {
 	 * @throws Exception
 	 */
 	public int insertMember(GuildMemberVO vo) throws Exception{
-		int count =sqlSession.getMapper(GuildDAO.class).insertMember(vo);
+		SqlSession sqlSession = getSqlSession();
+		int count =sqlSession .getMapper(GuildDAO.class).insertMember(vo);
 		if(count == 0)
 			throw new Exception("삽입 실패");
 		sqlSession.commit();
+		sqlSession.close();
 		return count;
 	}
 	/**
@@ -77,10 +87,12 @@ public class GuildService {
 	 * @throws Exception
 	 */
 	public int dropMember(GuildMemberVO vo) throws Exception{
-		int count =sqlSession.getMapper(GuildDAO.class).dropMember(vo);
+		SqlSession sqlSession = getSqlSession();
+		int count =sqlSession .getMapper(GuildDAO.class).dropMember(vo);
 		if(count == 0)
 			throw new Exception("삭제할 회원이 없습니다");
 		sqlSession.commit();
+		sqlSession.close();
 		return count;
 	}
 /**
@@ -95,7 +107,9 @@ public class GuildService {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("code", code);
 		map.put("date", date+"-%");
-		map = sqlSession.getMapper(GuildDAO.class).selectAllMemberMonthWarfaceScore(map);
+		SqlSession sqlSession = getSqlSession();
+		map = sqlSession .getMapper(GuildDAO.class).selectAllMemberMonthWarfaceScore(map);
+		sqlSession.close();
 		return map;
 	}
 	/**
@@ -110,12 +124,14 @@ public class GuildService {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("code", code);
 		map.put("date", date+"-%");
-		List<HashMap<String, Object>> list = sqlSession.getMapper(GuildDAO.class).selectMemberMonthWarfaceScore(map);
+		SqlSession sqlSession = getSqlSession();
+		List<HashMap<String, Object>> list = sqlSession .getMapper(GuildDAO.class).selectMemberMonthWarfaceScore(map);
 		MemberWarfaceScoreVO vo = new MemberWarfaceScoreVO();
 		vo.setCode(code);
 		vo.setDate(date);
 		vo.setList(list);
 		vo.setNick(selectMember(code).getNick());
+		sqlSession.close();
 		return vo;
 	}
 	
@@ -126,8 +142,10 @@ public class GuildService {
 	 * @throws Exception
 	 */
 	public List<HashMap<String, Object>> selectAllMemberMonthWarfaceAverageScore(String date) throws Exception {
+		SqlSession sqlSession = getSqlSession();
 		// TODO Auto-generated method stub
-		List<HashMap<String, Object>> list = sqlSession.getMapper(GuildDAO.class).selectAllMemberMonthWarfaceAverageScore(date+"-%");
+		List<HashMap<String, Object>> list = sqlSession .getMapper(GuildDAO.class).selectAllMemberMonthWarfaceAverageScore(date+"-%");
+		sqlSession.close();
 		return list;
 	}
 	
@@ -140,12 +158,13 @@ public class GuildService {
 	public ArrayList<MemberWarfaceScoreVO> selectAllMemberMonthWarfaceScore(String date) throws Exception{
 		ArrayList<MemberWarfaceScoreVO> list = new ArrayList<MemberWarfaceScoreVO>();
 		
-		List<Integer> currentMemberCodeList = sqlSession.getMapper(GuildDAO.class).selectCurrentMember();
+		SqlSession sqlSession = getSqlSession();
+		List<Integer> currentMemberCodeList = sqlSession .getMapper(GuildDAO.class).selectCurrentMember();
 		
 		for(int i=0;i<currentMemberCodeList.size();i++){
 			list.add(selectMonthWarfaceScore(currentMemberCodeList.get(i), date));			
 		}
-		
+		sqlSession.close();
 		return list;
 		
 	}
@@ -156,19 +175,26 @@ public class GuildService {
 	 * @return [{코드,닉네임,공성전점수}] 
 	 */
 	public List<HashMap<String, Object>> selectAllMemberDateWarface(String date){
-		return sqlSession.getMapper(GuildDAO.class).selectAllMemberDateWarface(date);
+		SqlSession sqlSession = getSqlSession();
+		List<HashMap<String, Object>> list =sqlSession .getMapper(GuildDAO.class).selectAllMemberDateWarface(date);
+		sqlSession.close();
+		return list;
 	}
 	
 	public ArrayList<String> selectDateNotWarfaceMember(String date) throws Exception{
 		List<HashMap<String, Object>> warfaceList = selectAllMemberDateWarface(date);
 		List<String> memberList = InstancePackage.nickMemberList;
 		
-		ArrayList<String> resultList = new ArrayList<String>(); 
+		ArrayList<String> resultList = new ArrayList<String>(memberList); 
 		System.out.println("회원 명단 : "+memberList+"인원수:"+memberList.size());
 		System.out.println("공성참석 명단 : "+warfaceList);
+		if(warfaceList.size()==0){
+			System.out.println("공성 아무도 안함");
+			return resultList;
+		}
 		for(int i=0;i<warfaceList.size();i++){
-			if(!memberList.contains(warfaceList.get(i).get("nick"))){
-				resultList.add(memberList.get(i));
+			if(resultList.contains(warfaceList.get(i).get("nick"))){
+				resultList.remove(warfaceList.get(i).get("nick"));
 			}
 		}
 		System.out.println("결과 : "+resultList);
@@ -178,7 +204,9 @@ public class GuildService {
 	public List<String> selectAllMemberNick(){
 		List<String> list = null;
 		try {
-			list = sqlSession.getMapper(GuildDAO.class).selectAllMemberNick();
+			SqlSession sqlSession = getSqlSession();
+			list = sqlSession .getMapper(GuildDAO.class).selectAllMemberNick();
+			sqlSession.close();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -188,8 +216,10 @@ public class GuildService {
 	}
 	
 	public List<HashMap<String, Object>> selectGuildWarInfo(String date) throws Exception{
+		SqlSession sqlSession=getSqlSession();
 		List<HashMap<String, Object>> list = sqlSession.getMapper(GuildDAO.class).selectGuildWarInfo(date);
 		System.out.println("selectGuildWarInfo : "+list);
+		sqlSession.close();
 		return list;
 	}
 
@@ -200,10 +230,13 @@ public class GuildService {
 		System.out.println("회원 리스트 : "+memberList);
 		System.out.println("참여 리스트 : " + guildwarList);
 		
-		ArrayList<String> resultList = new ArrayList<String>();
+		ArrayList<String> resultList = new ArrayList<String>(memberList);
+		if(guildwarList.size()==0){
+			return resultList;
+		}
 		for(int i=0;i<guildwarList.size();i++){
-			if(memberList.contains(guildwarList.get(i).get("nick"))){
-				resultList.add(memberList.get(i));
+			if(resultList.contains(guildwarList.get(i).get("nick"))){
+				resultList.remove(guildwarList.get(i).get("nick"));
 			}
 		}
 		System.out.println("결과 : "+resultList);
